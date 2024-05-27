72439f584951d782a2f224f998aadc1b75449f7d
jkc-mycode <jkc9709@naver.com>
2024-05-27 16:50:19 +0900

    Add search resume list api

--- src/routers/resumes.router.js
+++ src/routers/resumes.router.js
@@ -31,4 +31,28 @@ router.post('/resumes', authMiddleware, async (req, res, next) => {
     }
 });
 
+// 이력서 목록 조회 API
+router.get('/resumes', authMiddleware, async (req, res, next) => {
+    // 사용자 ID를 가져옴
+    const { userId } = req.user;
+    // 정렬 조건을 req.query로 가져옴
+    const sortType = req.query.sort.toLowerCase();
+
+    const resumes = await prisma.resumes.findMany({
+        where: { UserId: +userId },
+        select: {
+            resumeId: true,
+            UserName: true,
+            title: true,
+            introduce: true,
+            state: true,
+            createdAt: true,
+            updatedAt: true,
+        },
+        orderBy: { createdAt: sortType },
+    });
+
+    return res.status(200).json({ status: 200, message: '이력서 목록 조회에 성공했습니다.', data: { resumes } });
+});
+
 export default router;
