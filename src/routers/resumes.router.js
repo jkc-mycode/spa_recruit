34c299f0d9df62ecd442da4141e311008e27e6a9
jkc-mycode <jkc9709@naver.com>
2024-05-27 19:22:49 +0900

    Add search detail resume api

--- src/routers/resumes.router.js
+++ src/routers/resumes.router.js
@@ -9,8 +9,8 @@ const router = express.Router();
 //이력서 생성 API
 router.post('/resumes', authMiddleware, async (req, res, next) => {
     try {
-        // 사용자 ID와 이름을 가져옴
-        const { userId, name } = req.user;
+        // 사용자 ID를 가져옴
+        const { userId } = req.user;
         // 사용자가 입력한 제목과 자기소개에 대한 유효성 검사
         const validation = await resumeWriteSchema.validateAsync(req.body);
         const { title, introduce } = validation;
@@ -21,7 +21,6 @@ router.post('/resumes', authMiddleware, async (req, res, next) => {
                 title,
                 introduce,
                 UserId: +userId,
-                UserName: name,
             },
         });
 
@@ -32,7 +31,7 @@ router.post('/resumes', authMiddleware, async (req, res, next) => {
 });
 
 // 이력서 목록 조회 API
-router.get('/resumes', authMiddleware, async (req, res, next) => {
+router.get('/resumes', authMiddleware, async (req, res) => {
     // 사용자 ID를 가져옴
     const { userId } = req.user;
     // 정렬 조건을 req.query로 가져옴
@@ -42,7 +41,7 @@ router.get('/resumes', authMiddleware, async (req, res, next) => {
         where: { UserId: +userId },
         select: {
             resumeId: true,
-            UserName: true,
+            User: { select: { name: true } },
             title: true,
             introduce: true,
             state: true,
@@ -53,6 +52,33 @@ router.get('/resumes', authMiddleware, async (req, res, next) => {
     });
 
     return res.status(200).json({ status: 200, message: '이력서 목록 조회에 성공했습니다.', data: { resumes } });
+});
+
+// 이력서 상세 조회 API
+router.get('/resumes/:resumeId', authMiddleware, async (req, res) => {
+    // 사용자 ID를 가져옴
+    const { userId } = req.user;
+    // 이력서 ID를 가져옴
+    const { resumeId } = req.params;
+
+    // 이력서 ID, 작성자 ID가 모두 일치한 이력서 조회
+    const resume = await prisma.resumes.findFirst({
+        where: { resumeId: +resumeId, UserId: +userId },
+        select: {
+            resumeId: true,
+            User: { select: { name: true } },
+            title: true,
+            introduce: true,
+            state: true,
+            createdAt: true,
+            updatedAt: true,
+        },
+    });
+    if (!resume) {
+        return res.status(401).json({ status: 401, message: '이력서가 존재하지 않습니다.' });
+    }
+
+    return res.status(200).json({ status: 200, message: '이력서 상세 조회에 성공했습니다.', data: { resume } });
 });
 
 export default router;
