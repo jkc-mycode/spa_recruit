e42ef2e0e3795ba6905a9cf2419bc4a3979e5a15
jkc-mycode <jkc9709@naver.com>
2024-05-28 00:10:02 +0900

    Update auth middleware code

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 // AccessToken 인증 미들웨어
 export default async (req, res, next) => {
     try {
-        // 쿠키에서 Access 토큰 가져옴
+        // 헤더에서 Access 토큰 가져옴
         const authorization = req.headers['authorization'];
         console.log(req.headers);
         if (!authorization) throw new Error('인증 정보가 없습니다.');
@@ -20,7 +20,6 @@ export default async (req, res, next) => {
         // JWT에서 꺼낸 userId로 실제 사용자가 있는지 확인
         const user = await prisma.users.findFirst({ where: { userId: +userId } });
         if (!user) {
-            res.clearCookie('Authorization'); // 잘못된 쿠키이기 때문에 초기화
             return res.status(401).json({ status: 401, message: '인증 정보와 일치하는 사용자가 없습니다.' });
         }
 
@@ -29,7 +28,6 @@ export default async (req, res, next) => {
         // 다음 동작 진행
         next();
     } catch (err) {
-        res.clearCookie('Authorization'); // 잘못된 쿠키이기 때문에 초기화
         switch (err.name) {
             case 'TokenExpiredError':
                 return res.status(401).json({ status: 401, message: '인증 정보가 만료되었습니다.' });
