7bc3c68502e05cd1ceeb52f9cec3f7d7a1e4843d
jkc-mycode <jkc9709@naver.com>
2024-05-30 20:08:46 +0900

    Update search user info

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -8,19 +8,7 @@ const router = express.Router();
 
 // 내 정보 조회 API
 router.get('/users', authMiddleware, async (req, res) => {
-    const { userId } = req.user;
-
-    const user = await prisma.user.findFirst({
-        where: { userId },
-        select: {
-            userId: true,
-            email: true,
-            name: true,
-            role: true,
-            createdAt: true,
-            updatedAt: true,
-        },
-    });
+    const user = req.user;
 
     return res.status(HTTP_STATUS.OK).json({ status: HTTP_STATUS.OK, message: MESSAGES.USER.READ.SUCCEED, data: { user } });
 });
